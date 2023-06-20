f83f2c29789fdb7d18e888d5844f97a61340f7f0
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-19 21:03:49 -0700

    Improved pdf document layout by adjusting font sizes, font styles and cell heights

--- pdf_report.py
+++ pdf_report.py
@@ -19,17 +19,22 @@ class PDFReport:
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
         pdf.image("house.png", w=50, h=50)
+
         pdf.set_font(family='Arial', size=24, style='B')
         pdf.cell(w=0, h=80, txt="Roommates Bill", border=1, align='C', ln=1)
-        pdf.cell(w=100, h=40, txt="Period", border=1)
-        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+
+        pdf.set_font(family="Arial", size=18, style='B')
+        pdf.cell(w=100, h=40, txt="Period", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        pdf.set_font(family="Arial", size=14)
 
         for roommate in roommates:
             amount_due = str(roommate.get_amount_to_pay(bill, roommates))
-            pdf.cell(w=100, h=40, txt=roommate.name, border=1)
-            pdf.cell(w=150, h=40,
+            pdf.cell(w=100, h=25, txt=roommate.name, border=0)
+            pdf.cell(w=150, h=25,
                      txt=amount_due,
-                     border=1,
+                     border=0,
                      ln=1)
 
         pdf.output(f"{self.filename}.pdf")
