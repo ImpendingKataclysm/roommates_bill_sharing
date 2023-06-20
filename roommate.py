44709817158cc01ee583c8e252b57f58abe4343d
ImpendingKataclysm <ktjanzen42@gmail.com>
2023-06-19 20:52:46 -0700

    Round amount owed by Roommate to the nearest 100th to display reasonable dollar values

--- roommate.py
+++ roommate.py
@@ -8,11 +8,11 @@ class Roommate:
 
     def get_amount_to_pay(self, bill, roommates):
         """
-        Calculates the amount this Roommate owes for the given bill based on
+        Calculates the amount_due_usd this Roommate owes for the given bill based on
         how many days each roommate spent at home during the billing period.
         :param bill: the bill for which the period is calculated
         :param roommates: the list of Roommates living at the home
-        :return: the amount this Roommate owes for the billing period
+        :return: the amount_due_usd this Roommate owes for the billing period
         """
         roommates_days_home = 0
 
@@ -20,6 +20,6 @@ class Roommate:
             roommates_days_home += roommate.days_home
 
         weight = self.days_home / roommates_days_home
-        amount = bill.amount * weight
+        amount_due_usd = round(bill.amount * weight, 2)
 
-        return amount
+        return amount_due_usd
